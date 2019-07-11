b53aba2282375697167236b429d4e415ffc6fad2
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-07-11 18:19:40 +0300

    Updates

---
+++ precache-manifest.2817b2b9635c479d57343656e1f2c3cb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6923e493f8836dc516536f2df4d5fa3",
+    "url": "/react-giphy-search-wmreply/index.html"
+  },
+  {
+    "revision": "a3dd1078ab74e2df424d",
+    "url": "/react-giphy-search-wmreply/static/css/0.4ed99c55.chunk.css"
+  },
+  {
+    "revision": "a085dd05b6f63eed0223",
+    "url": "/react-giphy-search-wmreply/static/css/6.45c5f64b.chunk.css"
+  },
+  {
+    "revision": "75deefe1da645b407811",
+    "url": "/react-giphy-search-wmreply/static/css/giphy.59467123.chunk.css"
+  },
+  {
+    "revision": "ab74ba41bcd56e454bc6",
+    "url": "/react-giphy-search-wmreply/static/css/giphyitem.894dc1de.chunk.css"
+  },
+  {
+    "revision": "3cb562bc273f9f7e31ab",
+    "url": "/react-giphy-search-wmreply/static/css/main.947bba74.chunk.css"
+  },
+  {
+    "revision": "46619464fc11dd3744c6",
+    "url": "/react-giphy-search-wmreply/static/css/savegiphy.d07c1ca5.chunk.css"
+  },
+  {
+    "revision": "a3dd1078ab74e2df424d",
+    "url": "/react-giphy-search-wmreply/static/js/0.d6da984c.chunk.js"
+  },
+  {
+    "revision": "a085dd05b6f63eed0223",
+    "url": "/react-giphy-search-wmreply/static/js/6.e578c3d3.chunk.js"
+  },
+  {
+    "revision": "074d8b6f5860685a7201",
+    "url": "/react-giphy-search-wmreply/static/js/7.e60cc2c4.chunk.js"
+  },
+  {
+    "revision": "75deefe1da645b407811",
+    "url": "/react-giphy-search-wmreply/static/js/giphy.dbaa8c26.chunk.js"
+  },
+  {
+    "revision": "ab74ba41bcd56e454bc6",
+    "url": "/react-giphy-search-wmreply/static/js/giphyitem.32b72d3b.chunk.js"
+  },
+  {
+    "revision": "3cb562bc273f9f7e31ab",
+    "url": "/react-giphy-search-wmreply/static/js/main.a8a06d74.chunk.js"
+  },
+  {
+    "revision": "f27ea20cd22fc1972660",
+    "url": "/react-giphy-search-wmreply/static/js/runtime~main.21b59fd8.js"
+  },
+  {
+    "revision": "46619464fc11dd3744c6",
+    "url": "/react-giphy-search-wmreply/static/js/savegiphy.5f9f624b.chunk.js"
+  }
+]);
